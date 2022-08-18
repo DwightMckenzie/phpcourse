fa8c4feb6f1f4bf65a16c78aa68e5af852c14555
DwightMckenzie <dmckenzie2013@gmail.com>
2022-08-17 21:15:05 -0400

    I added two more menu items in navbar php

--- navbar.php
+++ navbar.php
@@ -13,6 +13,12 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
 
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
